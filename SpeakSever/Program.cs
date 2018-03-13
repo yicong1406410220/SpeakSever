8b273f26a330ed0d2ff0786038321cc403296a3d
azaz26753 <azazaz123456@foxmail.com>
2018-03-13 13:47:42 +0800

    添加项目文件。

---
+++ SpeakSever/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeakSever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string host = ConfigurationManager.AppSettings["host"];
+            int port = int.Parse(ConfigurationManager.AppSettings["port"]);
+            Console.WriteLine(host + " : " + port);
+            Serv serv = new Serv();
+            serv.Start(host, port);
+            while (true)
+            {
+                string str = Console.ReadLine();
+                switch (str)
+                {
+                    case "quit":
+                        return;
+                }
+            }
+
+
+        }
+    }
+}
---
+++ SpeakSever/Serv.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeakSever
+{
+    class Serv
+    {
+        //监听嵌套字
+        public Socket listenfd;
+        //客户端连接
+        public Conn[] conns;
+        //最大连接数
+        public int maxConn = 50;
+
+        //获取连接池索引，返回负数表示获取失败
+        public int NewIndex()
+        {
+            if (conns == null)
+                return -1;
+            for (int i = 0; i < conns.Length; i++)
+            {
+                if (conns[i] == null)
+                {
+                    conns[i] = new Conn();
+                    return i;
+                }
+                else if (conns[i].isUse == false)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        //开启服务器
+        public void Start(string host, int port)
+        {
+            //连接池
+            conns = new Conn[maxConn];
+            for (int i = 0; i < maxConn; i++)
+            {
+                conns[i] = new Conn();
+            }
+
+            //socket
+            listenfd = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //Bind
+            IPAddress ipAdr = IPAddress.Parse(host);
+            IPEndPoint ipEp = new IPEndPoint(ipAdr, port);
+            listenfd.Bind(ipEp);
+            //listen
+            listenfd.Listen(maxConn);
+            //accept
+            listenfd.BeginAccept(AcceptCb, null);
+            Console.WriteLine("[服务器]启动成功");
+
+
+        }
+        /// <summary>
+        /// Accept回调
+        /// </summary>
+        /// <param name="ar"></param>
+        private void AcceptCb(IAsyncResult ar)
+        {
+            try
+            {
+                Socket socket = listenfd.EndAccept(ar);
+                int index = NewIndex();
+
+                if (index < 0)
+                {
+                    socket.Close();
+                    Console.WriteLine("警告连接池已满");
+                }
+                else
+                {
+                    Conn conn = conns[index];
+                    conn.Init(socket);
+                    string adr = conn.GetAdress();
+                    Console.WriteLine("客户端连接[" + adr + "] conn池id:" + index);
+                    conn.socket.BeginReceive(conn.readBuff, conn.buffCount, conn.BuffRemain(), SocketFlags.None, ReceiveCb, conn);
+                    listenfd.BeginAccept(AcceptCb, null);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("AcceptCb失败:" + e.Message);
+
+            }
+        }
+
+
+        private void ReceiveCb(IAsyncResult ar)
+        {
+            Conn conn = (Conn)ar.AsyncState;
+            try
+            {
+                int count = conn.socket.EndReceive(ar);
+                //关闭信号
+                if (count <= 0)
+                {
+                    Console.WriteLine("收到[" + conn.GetAdress() + "]断开连接");
+                    conn.Close();
+                    return;
+                }
+                //数据处理
+                string str = System.Text.Encoding.UTF8.GetString(conn.readBuff, 0, count);
+                Console.WriteLine("收到 [" + conn.GetAdress() + "] 数据" + str);
+                byte[] bytes = System.Text.Encoding.UTF8.GetBytes(str);
+                //广播
+                for (int i=0; i < conns.Length; i++)
+                {
+                    if (conns[i] == null)
+                        continue;
+                    if (!conns[i].isUse)
+                        continue;
+                    Console.WriteLine("将消息转播给" + conns[i].GetAdress());
+                    conns[i].socket.Send(bytes);
+                }
+                //继续接收
+                conn.socket.BeginReceive(conn.readBuff, conn.buffCount, conn.BuffRemain(), SocketFlags.None, ReceiveCb, conn);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("收到 [" + conn.GetAdress() + "] 断开连接");
+                conn.Close();
+            }
+
+
+        }
+
+    }
+}
